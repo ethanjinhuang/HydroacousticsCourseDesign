867539055d1a6fd701a2bd9612c17444e48158ee
GimHuang <kim.huang.j@qq.com>
2020-06-29 09:00:48 +0800

    remove 全球argo散点资料集

--- Program/main.m
+++ Program/main.m
@@ -1,4 +1,9 @@
 clear;
 clc;
+tic;
 
-[eng,pos,data] = read_data_from_single_file("..\\DATA\\201912\\1900978_370.dat");
+[eng,pos,data] = read_data_from_single_file("..\\DATA\\201912\\1900979_370.dat");
+
+toc;
+
+%disp(['reading finish! total cost']);
